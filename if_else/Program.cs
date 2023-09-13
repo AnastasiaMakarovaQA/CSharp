853ae9b646d4f47aba4455fe9bbe48a4de170d3c
Anastasia <mnastasss@gmail.com>
2023-09-13 17:44:49 +0300

    if_else added

---
+++ if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя");
+string username = Console.ReadLine();
+
+if(username == " Кирилл")
+{
+Console.WriteLine("Привет, Любовь моя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
