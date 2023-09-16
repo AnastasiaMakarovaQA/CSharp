5935295e73f44c67e5a4cb177a587d01017d4847
Anastasia <mnastasss@gmail.com>
2023-09-17 01:10:05 +0300

    array_task added

---
+++ array_task/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 34, 75, 37, 98, 234, 985, 35};
+int n = array.Length;
+int find = 37;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index); 
+           }    
+    index++;
+}
